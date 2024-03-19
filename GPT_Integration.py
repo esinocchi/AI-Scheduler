c85fbfc1bce8e62cc0ea2370ca90e36dd7f715de
esinocchi <evan.sinocchi@gmail.com>
2024-03-18 23:40:24 -0400

    Update GPT_Integration.py

--- GPT_Integration.py
+++ GPT_Integration.py
@@ -8,6 +8,7 @@ def requestAI(semester, taken_courses, preferences):
     load_dotenv()
     OPENAI_API_KEY = os.getenv('MY_API_KEY')
     client = OpenAI(api_key = OPENAI_API_KEY)
+    #test
 
 
     # semester = '0'
